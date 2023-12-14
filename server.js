647442452fabbe7c624294d7832f5d541d83daed
MOULOUD CHABOUNI <mouloud619@gmail.com>
2023-12-13 18:35:12 -0600

    commit

--- server.js
+++ server.js
@@ -55,6 +55,26 @@ app.get('/tips/:total/:percentage',(req,res) => {
 
 
 
+// Magic 8 Ball responses 
+const magic8BallResponses = [
+    "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+    "You may rely on it", "As I see it yes", "Most likely", "Outlook good",
+    "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later",
+    "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
+    "Don't count on it", "My reply is no", "My sources say no",
+    "Outlook not so good", "Very doubtful"
+  ];
+
+// create route for magic 8 ball
+app.get('/magic/:question',(req,res) => {
+    const question = req.params.question;
+    const randomResponse = magic8BallResponses[Math.floor(Math.random() * magic8BallResponses.length)];
+    res.send(`<h1>Question: ${question}</h1><h1>Answer: ${randomResponse}</h1>`);
+
+})
+
+
+
 
 
 
---
+++ views/magic.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function magic() {
+  return (
+    <div>magic</div>
+  )
+}
+
+export default magic
